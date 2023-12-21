73b6acb677d5a5d2c4f0adad85a44fa68062569e
siva shanmugam <sivashanmugam.swaminathan@nokia.com>
2023-12-21 19:32:21 +0530

    Full functionality

--- vars/pushtoJfrog.groovy
+++ vars/pushtoJfrog.groovy
@@ -1,3 +1,8 @@
-def call() {
-    echo "I am pushing this to artifactory"
+def call(String artifactory) {
+    sh """
+        mvn clean install -D skipTests
+        pwd
+        cd Java_app_3.0/target
+        curl -X PUT -u admin -p "Nokia?123" -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
+    """
 }
