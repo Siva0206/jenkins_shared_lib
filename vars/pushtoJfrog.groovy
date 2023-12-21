c67242ef161b97697e9d2ce5cf774ded64d5271c
siva shanmugam <sivashanmugam.swaminathan@nokia.com>
2023-12-21 19:48:38 +0530

    This is shivas try

--- vars/pushtoJfrog.groovy
+++ vars/pushtoJfrog.groovy
@@ -1,8 +1,7 @@
 def call(String artifactory) {
     sh """
-        mvn clean install -D skipTests
         pwd
         cd Java_app_3.0/target
-        curl -X PUT -u admin -p "Nokia?123" -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
+        curl -X PUT -u "admin":"Nokia?123" -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
     """
 }
