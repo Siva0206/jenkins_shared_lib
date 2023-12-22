9ed8f5f31a4eac9b14439bcdb9b90188e4e3c68e
siva shanmugam <sivashanmugam.swaminathan@nokia.com>
2023-12-22 11:13:17 +0530

    just try

--- vars/pushtoJfrog.groovy
+++ vars/pushtoJfrog.groovy
@@ -1,5 +1,5 @@
 def call(String artifactory) {
     sh """
-        curl -X PUT -u "admin":"Nokia?123" -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
+        curl -X PUT -u "admin":"Nokia?123" -T target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
     """
 }
