21d89e13f2efcea9ece09eb807d3866879930f43
siva shanmugam <sivashanmugam.swaminathan@nokia.com>
2023-12-22 10:54:27 +0530

    Fixing the directory switching

--- vars/pushtoJfrog.groovy
+++ vars/pushtoJfrog.groovy
@@ -1,7 +1,7 @@
 def call(String artifactory) {
     sh """
         pwd
-        cd Java_app_3.0/target
+        cd target
         curl -X PUT -u "admin":"Nokia?123" -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
     """
 }
