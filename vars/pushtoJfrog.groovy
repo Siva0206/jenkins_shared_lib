dbdd6690673edbe440932219bf705b25e1264c09
siva shanmugam <sivashanmugam.swaminathan@nokia.com>
2023-12-22 11:13:17 +0530

    just try

--- vars/pushtoJfrog.groovy
+++ vars/pushtoJfrog.groovy
@@ -1,5 +1,6 @@
 def call(String artifactory) {
     sh """
+        ls -lrht
         curl -X PUT -u "admin":"Nokia?123" -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${artifactory}:8082/artifactory/example-repo-local
     """
 }
